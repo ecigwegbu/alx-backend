6e41df0fbc50299b08b9b5a04c9b7f9e94cbd705
ecigwegbu <igwegbu@gmail.com>
2023-07-23 07:19:38 +0100

    0. Simple helper function - with neg checks/w typing

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """0. Simple helper function"""
-import typing
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Write a function named index_range that takes two integer arguments
     page and page_size.
 

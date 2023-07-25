318b87467ede69e5d7807c86f6c3ac927b6a23f1
ecigwegbu <igwegbu@gmail.com>
2023-07-25 18:07:30 +0100

    task1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,33 +1,43 @@
 #!/usr/bin/env python3
 """0. Basic dictionary"""
-from sys import maxsize
 from base_caching import BaseCaching
 
 
-class BasicCache(BaseCaching):
-    """Create a class BasicCache that inherits from BaseCaching and is a
+class FIFOCache(BaseCaching):
+    """Create a class FIFOCache that inherits from BaseCaching and is a
     caching system:
 
     You must use self.cache_data - dictionary from the parent class
     BaseCaching
-    This caching system doesn’t have limit
+    You can overload def __init__(self): but don’t forget to call the parent
+    init: super().__init__()
     def put(self, key, item):
     Must assign to the dictionary self.cache_data the item value for the key
     key.
     If key or item is None, this method should not do anything.
+    If the number of items in self.cache_data is higher than
+    BaseCaching.MAX_ITEMS:
+    you must discard the first item put in cache (FIFO algorithm)
+    you must print DISCARD: with the key discarded and following by a new line
     def get(self, key):
     Must return the value in self.cache_data linked to key.
-    If key is None or if the key doesn’t exist in self.cache_data, return None
+    If key is None or if the key doesn’t exist in self.cache_data, return None.
     """
     def __init__(self):
         super().__init__()
-        self.MAX_ITEMS = maxsize
+        # self.MAX_ITEMS = maxsize
 
     def put(self, key, item):
         """Add an item in the cache. Overides not-implemented base class
         function with same name"""
         if key is not None:
-            self.cache_data.update({key: item})  # [key] = item
+            if len(self.cache_data) < self.MAX_ITEMS:
+                self.cache_data.update({key: item})
+            else:
+                discarded_item_key = next(iter(self.cache_data))
+                self.cache_data.pop(discarded_item_key)
+                print("DISCARD: {}".format(discarded_item_key))
+                self.cache_data.update({key: item})
 
     def get(self, key):
         """ Get an item by key. Overides not-implemented base class

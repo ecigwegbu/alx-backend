79aae6a636545eb42a36782f256049eacda12719
ecigwegbu <igwegbu@gmail.com>
2023-08-01 18:04:31 +0100

    task3 -jina2 issue

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,18 +1,37 @@
 #!/usr/bin/env python3
-"""0. Basic Flask app. Outputs Hello World."""
-from flask import Flask, render_template
+"""3. Parameterize templates - outputs Hello World."""
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
 from datetime import datetime, date, time, timedelta
 # import requests
 # from os import getenv
 
 
+class Config(object):
+    """Config file for flask_babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
 app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Get the best match locale for the user
+    Uses the info in the request header and the config"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route("/")
-def basic_flask_app():
-    """Basic Hello World Flask app"""
-    return render_template('0-index.html')
+def parameterize_template():
+    """Basic Basic Babel Flask app"""
+
+    return render_template('3-index.html', home_title=_('Welcome to Holberton'),
+                           home_header=_('Hello World'))
 
 
 if __name__ == "__main__":

2b363cd8eb22652e0a240ad94f4d7dc5c8122560
ecigwegbu <igwegbu@gmail.com>
2023-08-02 12:31:44 +0100

    docs

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""3. Parameterize templates - outputs Hello World."""
+"""3. Parameterize templates - outputs Hello World.
+This module parametises the title and header to different
+locales"""
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
 from datetime import datetime, date, time, timedelta
@@ -23,13 +25,15 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale() -> Union[str, None]:
     """Get the best match locale for the user
-    Uses the info in the request header and the config"""
+    Uses the info in the request header and the config. If locale not
+    found then return None"""
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route("/")
 def parameterize_template() -> str:
-    """Basic Basic Babel Flask app"""
+    """Basic Basic Babel Flask app. Returns the index page containing
+    The title and header translated to the locale of choice"""
     home_title = _("Welcome to Holberton")
     home_header = _("Hello world!")
     return render_template("3-index.html", home_title=home_title,
